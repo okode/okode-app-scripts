d10e10ab6d616929afa40640a640b72e84790eab
alexdiaz <adiaz@okode.com>
2017-12-02 11:25:09 +0100

    Added component, directive and pipe generators

--- src/generator.ts
+++ src/generator.ts
@@ -1,6 +1,9 @@
 import * as fs from 'fs';
 
+// Generator --------------------------------------------------------------------------------------
+
 module Generator {
+
   export function init(args: any) {
     if (args.length < 4) {
       Utils.showErrorInvalidArgs();
@@ -11,7 +14,7 @@ module Generator {
       else Utils.showErrorInvalidArgs();
     }
   }
-  // private
+
   const fs = require('fs');
   const util = require('util');
   const read = util.promisify(fs.readFile);
@@ -19,77 +22,133 @@ module Generator {
   const tmplDir = __dirname + '/templates';
   let type: string;
   let name: string;
+
   function generate() {
     switch (type) {
       case 'page':      generatePage(); break;
       case 'component': generateComponent(); break;
+      case 'directive': generateDirective(); break;
       case 'pipe':      generatePipe(); break;
       case 'service':   generateService(); break;
-      case 'directive': generateDirective(); break;
     }
   }
+
+  // page
+
   async function generatePage() {
     Utils.mkdir('./pages');
     Utils.mkdir(`./pages/${name}`);
-    // create page.ts
+    // .ts
     let tmpl = await read(`${tmplDir}/page/ts.tmpl`);
     await write(`./pages/${name}/${name}.ts`, Utils.replaceTmpl(tmpl, name));
-    // create page.module.ts
+    // .module.ts
     tmpl = await read(`${tmplDir}/page/module.ts.tmpl`);
     await write(`./pages/${name}/${name}.module.ts`, Utils.replaceTmpl(tmpl, name));
-    // create page.html
+    // .html
     tmpl = await read(`${tmplDir}/page/html.tmpl`);
     await write(`./pages/${name}/${name}.html`, Utils.replaceTmpl(tmpl, name));
-    // create page.scss
+    // .scss
     tmpl = await read(`${tmplDir}/page/scss.tmpl`);
     await write(`./pages/${name}/${name}.scss`, Utils.replaceTmpl(tmpl, name));
-    // complete
+    //
     Utils.showSuccess(type, name);
   }
-  function generateComponent() {
-    // TODO
-    Utils.showError('TODO generateComponent');
+
+  // component
+
+  async function generateComponent() {
+    Utils.mkdir('./components');
+    Utils.mkdir(`./components/${name}`);
+    // .ts
+    let tmpl = await read(`${tmplDir}/component/ts.tmpl`);
+    await write(`./components/${name}/${name}.ts`, Utils.replaceTmpl(tmpl, name));
+    // .module.ts
+    tmpl = await read(`${tmplDir}/component/module.ts.tmpl`);
+    await write(`./components/${name}/${name}.module.ts`, Utils.replaceTmpl(tmpl, name));
+    // .html
+    tmpl = await read(`${tmplDir}/component/html.tmpl`);
+    await write(`./components/${name}/${name}.html`, Utils.replaceTmpl(tmpl, name));
+    // .scss
+    tmpl = await read(`${tmplDir}/component/scss.tmpl`);
+    await write(`./components/${name}/${name}.scss`, Utils.replaceTmpl(tmpl, name));
+    //
+    Utils.showSuccess(type, name);
   }
-  function generatePipe() {
-    // TODO
-    Utils.showError('TODO generatePipe');
+
+  // directive
+
+  async function generateDirective() {
+    Utils.mkdir('./directives');
+    Utils.mkdir(`./directives/${name}`);
+    // .ts
+    let tmpl = await read(`${tmplDir}/directive/ts.tmpl`);
+    await write(`./directives/${name}/${name}.ts`, Utils.replaceTmpl(tmpl, name));
+    // .module.ts
+    tmpl = await read(`${tmplDir}/directive/module.ts.tmpl`);
+    await write(`./directives/${name}/${name}.module.ts`, Utils.replaceTmpl(tmpl, name));
+    //
+    Utils.showSuccess(type, name);
   }
-  function generateService() {
-    // TODO
+
+  // pipe
+
+  async function generatePipe() {
+    Utils.mkdir('./pipes');
+    Utils.mkdir(`./pipes/${name}`);
+    // .ts
+    let tmpl = await read(`${tmplDir}/pipe/ts.tmpl`);
+    await write(`./pipes/${name}/${name}.ts`, Utils.replaceTmpl(tmpl, name));
+    // .module.ts
+    tmpl = await read(`${tmplDir}/pipe/module.ts.tmpl`);
+    await write(`./pipes/${name}/${name}.module.ts`, Utils.replaceTmpl(tmpl, name));
+    //
+    Utils.showSuccess(type, name);
+  }
+
+  // service
+
+  async function generateService() {
     Utils.showError('TODO generateService');
   }
-  function generateDirective() {
-    // TODO
-    Utils.showError('TODO generateDirective');
-  }
+
 }
 
+// Utils ------------------------------------------------------------------------------------------
+
 module Utils {
-  const types = [ 'page', 'component', 'pipe', 'service', 'directive' ];
+
+  const types = [ 'page', 'component', 'directive', 'pipe', 'service' ];
   const c = require('colors');
+
   export function isValidType(type: string) {
     return types.indexOf(type) >= 0;
   }
+
   export function mkdir(path: string) {
     if (!fs.existsSync(path)) { fs.mkdirSync(path); }
   }
+  
   export function showError(err: any) {
     console.log('\n' + c.red('ERROR: ' + err) + '\n');
   }
+
   export function showErrorInvalidArgs() {
     console.log('\n' + c.red('ERROR: Invalid args'));
     console.log('       Usage example: ' + c.green('generator TYPE NAME'));
     console.log('       TYPE must be one of: ' + c.green(types.join(', ')));
     console.log('       NAME must be lowercase with - separating words: looks-like-this\n');
   }
+
   export function showSuccess(type: string, name: string) {
     console.log('\n' + c.green('Success!'));
     console.log('Generated a ' + c.green(type) +' named ' + c.green(name) + '\n');
   }
+
   export function snakeToCamelCase(s: string){
     let r = s.replace(/(\-\w)/g, function(m) { return m[1].toUpperCase(); });
     return r.charAt(0).toUpperCase() + r.slice(1);
   }
+
   export function replaceTmpl(content: any, name: string) {
     let className = Utils.snakeToCamelCase(name);
     content = content.toString();
@@ -97,6 +156,9 @@ module Utils {
     content = content.replace(/\$CLASSNAME\$/gi, className);
     return content;
   }
+
 }
+
+// ------------------------------------------------------------------------------------------------
 
 Generator.init(process.argv);
