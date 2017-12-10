8c0480e75875cc4eb79f2ff9f1ea44aa60cfd2db
Pedro Jorquera <pjorquera@okode.com>
2017-12-10 20:26:37 +0100

    Refactor

--- config/webpack.ifdef.config.js
+++ config/webpack.ifdef.config.js
@@ -1,21 +1,14 @@
 const webpackConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
-webpackConfig.dev.module.loaders.unshift({
-  loader: 'ifdef-loader',
-  options: {
-    PROD: process.env.IONIC_ENV === 'prod',
-    DEV: process.env.IONIC_ENV === 'dev',
-    "ifdef-verbose": true
-  }
-});
-
-webpackConfig.prod.module.loaders.unshift({
-  loader: 'ifdef-loader',
-  options: {
-    PROD: process.env.IONIC_ENV === 'prod',
-    DEV: process.env.IONIC_ENV === 'dev',
-    "ifdef-verbose": true
-  }
+Object.keys(webpackConfig).forEach(function(env) {
+  webpackConfig[env].module.loaders.unshift({
+    loader: 'ifdef-loader',
+    options: {
+      PROD: process.env.IONIC_ENV === 'prod',
+      DEV:  process.env.IONIC_ENV === 'dev',
+      "ifdef-verbose": true
+    }
+  });
 });
 
 console.log('DEBUG WEBPACK: Current loaders: ' + JSON.stringify(webpackConfig.dev.module.loaders));
