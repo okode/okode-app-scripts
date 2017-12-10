10acaccb5941cbebd98956485c74d604fca113e4
Pedro Jorquera <pjorquera@okode.com>
2017-12-10 20:20:15 +0100

    Apply ifdef plugin for dev and prod configurations

--- config/webpack.ifdef.config.js
+++ config/webpack.ifdef.config.js
@@ -1,11 +1,20 @@
 const webpackConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
-// webpackConfig.[dev | prod].module.loaders are the same object, we can use any of them
 webpackConfig.dev.module.loaders.unshift({
   loader: 'ifdef-loader',
   options: {
     PROD: process.env.IONIC_ENV === 'prod',
-    DEV: process.env.IONIC_ENV === 'dev'
+    DEV: process.env.IONIC_ENV === 'dev',
+    "ifdef-verbose": true
+  }
+});
+
+webpackConfig.prod.module.loaders.unshift({
+  loader: 'ifdef-loader',
+  options: {
+    PROD: process.env.IONIC_ENV === 'prod',
+    DEV: process.env.IONIC_ENV === 'dev',
+    "ifdef-verbose": true
   }
 });
 
