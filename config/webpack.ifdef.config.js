306d470db7fc8970a36f09f194da3ed49cfaa898
Pedro Jorquera <pjorquera@okode.com>
2017-12-10 22:06:31 +0100

    Forced processing ifdef comments for ts files

--- config/webpack.ifdef.config.js
+++ config/webpack.ifdef.config.js
@@ -2,6 +2,7 @@ const webpackConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
 Object.keys(webpackConfig).forEach(function(env) {
   webpackConfig[env].module.loaders.unshift({
+    test: /\.ts$/,
     loader: 'ifdef-loader',
     options: {
       PROD: process.env.IONIC_ENV === 'prod',
