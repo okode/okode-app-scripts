f1f5d0b9d9f04e706563954e6b68b710694e2a29
Pedro Jorquera <pjorquera@okode.com>
2018-02-25 23:11:00 +0100

    Using readFileSync instead require

--- dist/fetch-plugins.js
+++ dist/fetch-plugins.js
@@ -4,7 +4,8 @@ var fs = require("fs-extra");
 var FetchPlugins;
 (function (FetchPlugins) {
     function init() {
-        var fetch = require('./plugins/fetch.json');
+        fs.readFile;
+        var fetch = JSON.parse(fs.readFileSync('plugins/fetch.json', 'utf8'));
         for (var plugin in fetch) {
             console.log("Fetching plugin: " + plugin);
             fs.copySync("node_modules/" + plugin, "plugins/" + plugin);
--- src/fetch-plugins.ts
+++ src/fetch-plugins.ts
@@ -3,7 +3,8 @@ import * as fs from 'fs-extra';
 export module FetchPlugins {
 
   export function init() {
-    let fetch = require('./plugins/fetch.json');
+    fs.readFile
+    let fetch = JSON.parse(fs.readFileSync('plugins/fetch.json', 'utf8'));
     for (let plugin in fetch) {
       console.log(`Fetching plugin: ${plugin}`);
       fs.copySync(`node_modules/${plugin}`, `plugins/${plugin}`);
