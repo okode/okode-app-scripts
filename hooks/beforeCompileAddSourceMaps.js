c3669991a863681e4497006e83ccd31b7e67b81d
Pedro Jorquera <pjorquera@okode.com>
2018-03-28 17:29:06 +0200

    Added support for source maps in Cordova 8 projects

--- hooks/beforeCompileAddSourceMaps.js
+++ hooks/beforeCompileAddSourceMaps.js
@@ -3,30 +3,59 @@ var path = require('path');
 
 module.exports = function(context) {
 
-    // Do nothing for debug builds (Ionic already add sourcemaps to debug builds)
-    if (!context.opts.options.release) return;
+    let projectRoot = context.opts.projectRoot;
+    let cordovaMajor = parseInt(context.opts.cordova.version.charAt(0));
 
-    var projectRoot = context.opts.projectRoot;
+    if (cordovaMajor >= 8) {
 
-    for (let platform of context.opts.platforms) {
-        
-        // Compute destination folder
-        let destinationDir = 'platforms/ios/www/build';
-        if (platform == 'android') {
-          destinationDir = 'platforms/android/assets/www/build';
-          if (!fs.existsSync(destinationDir)) {
+      for (let platform of context.opts.platforms) {
+          
+          // Compute destination folder
+          let destinationDir = 'platforms/ios/www/build';
+          if (platform == 'android') {
             destinationDir = 'platforms/android/app/src/main/assets/www/build';
           }
-        }
 
-        // Copy source map to destination folder
-        let sourceMainJSMap = path.join(projectRoot, '.sourcemaps/main.js.map');
-        let destinationMainJSMap = path.join(projectRoot, destinationDir + '/main.js.map');
-        fs.writeFileSync(destinationMainJSMap, fs.readFileSync(sourceMainJSMap));
+          let buildDir = path.join(projectRoot, destinationDir);
+          fs.readdirSync(buildDir).filter(file => file.endsWith('.js')).forEach(jsFile => {
+            let jsFullPath = path.join(buildDir, jsFile);
+            let jsMapFullPath = jsFullPath + '.map';
+            if (fs.existsSync(jsMapFullPath)) {
+              let data = fs.readFileSync(jsFullPath);
+              if (data.indexOf('//# sourceMappingURL=') == -1) {
+                console.log('Adding missing source mapping URL: ' + jsFullPath);
+                fs.appendFileSync(jsFullPath, '//# sourceMappingURL=' + jsFile + '.map');
+              }
+            }
+          });
+          
+      }
 
-        // Append source mapping URL comment to main.js
-        let mainJS = path.join(projectRoot, destinationDir + '/main.js');
-        fs.appendFileSync(mainJS, '//# sourceMappingURL=main.js.map');
+    } else {
+
+      // Do nothing for debug builds (Ionic already add sourcemaps to debug builds)
+      if (!context.opts.options.release) return;
+
+      for (let platform of context.opts.platforms) {
+          
+          // Compute destination folder
+          let destinationDir = 'platforms/ios/www/build';
+          if (platform == 'android') {
+            destinationDir = 'platforms/android/assets/www/build';
+            if (!fs.existsSync(destinationDir)) {
+              destinationDir = 'platforms/android/app/src/main/assets/www/build';
+            }
+          }
+
+          // Copy source map to destination folder
+          let sourceMainJSMap = path.join(projectRoot, '.sourcemaps/main.js.map');
+          let destinationMainJSMap = path.join(projectRoot, destinationDir + '/main.js.map');
+          fs.writeFileSync(destinationMainJSMap, fs.readFileSync(sourceMainJSMap));
+
+          // Append source mapping URL comment to main.js
+          let mainJS = path.join(projectRoot, destinationDir + '/main.js');
+          fs.appendFileSync(mainJS, '//# sourceMappingURL=main.js.map');
+      }
 
     }
 
